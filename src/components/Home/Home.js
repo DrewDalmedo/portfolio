ef03c07d0ef448013da18a529a63b78a4123f64c
DrewDalmedo <andrewdalmedo@gmail.com>
2020-10-15 12:48:10 -0400

    Add projects component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,19 @@
 import React from 'react'
+
 import Container from 'react-bootstrap/Container'
+/*
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+*/
+
 import Hero from '../Hero/Hero'
+import Projects from '../Projects/Projects'
 
 const Home = () => {
   return (
     <Container className="p-0" fluid>
       <Hero />
+      <Projects />
     </Container>
   )
 }
