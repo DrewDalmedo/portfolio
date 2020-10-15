796789dbfd79cd5a98a6e182cee20ebdbd92c8f4
DrewDalmedo <andrewdalmedo@gmail.com>
2020-10-15 11:30:56 -0400

    Create hard-coded hero component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+const Hero = props => {
+  return (
+    <Jumbotron className="bg-transparent p-0" fluid>
+      <Container fluid>
+        <Row className="justify-content-center py-5">
+          <Col md={10} sm={12}>
+            <h1 className="d-flex justify-content-center display-1 font-weight-bolder">Drew Dalmedo</h1>
+            <h3 className="d-flex justify-content-center display-4">Code that matters</h3>
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
+  )
+}
+
+export default Hero
