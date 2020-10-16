3edc889037a653ea90f9034a9015094404dfc58e
DrewDalmedo <andrewdalmedo@gmail.com>
2020-10-15 20:23:40 -0400

    Remove projects path and add blog path

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path='/about' component={About} />
-        <Route path='/projects' component={Projects} />
+        <Route path='/blog' component={Projects} />
         <Route path='/contact' component={Contact} />
       </Switch>
     </Router>
