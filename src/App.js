d682f25f355c2933d758058b3d74dd2b3b485f25
DrewDalmedo <andrewdalmedo@gmail.com>
2020-10-15 20:42:16 -0400

    Change component rendered at /blog to the Blog component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 // components
 import Home from './components/Home/Home'
 import About from './components/About/About'
-import Projects from './components/Projects/Projects'
+import Blog from './components/Blog/Blog'
 import Contact from './components/Contact/Contact'
 
 import Navigation from './components/Navbar/Navigation'
@@ -17,7 +17,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path='/about' component={About} />
-        <Route path='/blog' component={Projects} />
+        <Route path='/blog' component={Blog} />
         <Route path='/contact' component={Contact} />
       </Switch>
     </Router>
