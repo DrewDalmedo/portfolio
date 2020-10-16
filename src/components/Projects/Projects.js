998aa86620bff8857e40da39203def3b23c4e78e
DrewDalmedo <andrewdalmedo@gmail.com>
2020-10-16 17:46:35 -0400

    Add header text to projects list

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,20 +5,25 @@ import ProjectCard from '../Projects/ProjectCard'
 
 const Projects = () => {
   return (
-    <Row className="justify-content-center">
-      <ProjectCard 
-        title="Flatiron Jobs V2" 
-        quickdesc="A job finding web app, built with React and Rails." 
-        codelink="https://github.com/DrewDalmedo/flatiron-jobs-v2" 
-        videolink="https://youtu.be/vCfOmSKjWk0"
-      />
-      <ProjectCard 
-        title="VGC" 
-        quickdesc="An organizational web app for video game collections."
-        codelink="https://github.com/DrewDalmedo/VGC"
-        videolink="https://youtu.be/9L8we0b4iHs"
-      />
-    </Row>
+    <div>
+      <Row className="justify-content-center">
+        <p className="font-weight-light" style={{fontSize: "24px"}}>My latest projects:</p>
+      </Row>
+      <Row className="justify-content-center">
+        <ProjectCard 
+          title="Flatiron Jobs V2" 
+          quickdesc="A job finding web app, built with React and Rails." 
+          codelink="https://github.com/DrewDalmedo/flatiron-jobs-v2" 
+          videolink="https://youtu.be/vCfOmSKjWk0"
+        />
+        <ProjectCard 
+          title="VGC" 
+          quickdesc="An organizational web app for video game collections."
+          codelink="https://github.com/DrewDalmedo/VGC"
+          videolink="https://youtu.be/9L8we0b4iHs"
+        />
+      </Row>
+    </div>
   )
 }
 
