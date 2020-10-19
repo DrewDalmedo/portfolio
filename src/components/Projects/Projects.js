4347e9c254b7c6f4d611c8f0f50559d722e225b6
DrewDalmedo <andrewdalmedo@gmail.com>
2020-10-19 11:47:08 -0400

    Import and implement project thumbnail images

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,9 @@ import React from 'react'
 import Row from 'react-bootstrap/Row'
 import ProjectCard from '../Projects/ProjectCard'
 
+import fjv2 from '../../assets/fjv2.png'
+import vgc from '../../assets/vgc.png'
+
 const Projects = () => {
   return (
     <div>
@@ -11,12 +14,14 @@ const Projects = () => {
       </Row>
       <Row className="justify-content-center">
         <ProjectCard 
+          image={fjv2}
           title="Flatiron Jobs V2" 
           quickdesc="A job finding web app, built with React and Rails." 
           codelink="https://github.com/DrewDalmedo/flatiron-jobs-v2" 
           videolink="https://youtu.be/vCfOmSKjWk0"
         />
         <ProjectCard 
+          image={vgc}
           title="VGC" 
           quickdesc="An organizational web app for video game collections."
           codelink="https://github.com/DrewDalmedo/VGC"
