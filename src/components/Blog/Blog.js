7ebf62a66b35aa252cba01b8d578b859a1a40b5c
DrewDalmedo <andrewdalmedo@gmail.com>
2021-01-26 12:52:48 -0500

    Refactor blog to conform to the site's design language

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,17 +1,12 @@
 import React from 'react'
 
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Hero from '../Hero/Hero'
 
 const blog = () => {
   return (
-    <Container fluid>
-      <Row className="justify-content-center">
-        <h1 className="text-center display-1">No blog posts yet! </h1>
-        <span className="text-center display-1" role="img" aria-label="sad face">😢</span>
-      </Row>
-    </Container>
+    <div>
+      <Hero title="Blogs coming soon!" />
+    </div>
   )
 }
 
