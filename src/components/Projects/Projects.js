aae8fd7dc476fad1c9de19da66f3051aaec386a8
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-25 11:59:52 -0500

    Add typos project card

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import ProjectCard from '../Projects/ProjectCard'
 
 import fjv2 from '../../assets/fjv2.png'
 import vgc from '../../assets/vgc.png'
+import placeholder from '../../assets/placeholder.svg'
 
 const Projects = () => {
   return (
@@ -18,14 +19,21 @@ const Projects = () => {
           title="Flatiron Jobs V2" 
           quickdesc="A job finding web app, built with React and Rails." 
           codelink="https://github.com/DrewDalmedo/flatiron-jobs-v2" 
-          videolink="https://youtu.be/vCfOmSKjWk0"
+          demolink="https://youtu.be/vCfOmSKjWk0"
         />
         <ProjectCard 
           image={vgc}
           title="VGC" 
           quickdesc="An organizational web app for video game collections."
           codelink="https://github.com/DrewDalmedo/VGC"
-          videolink="https://youtu.be/9L8we0b4iHs"
+          demolink="https://youtu.be/9L8we0b4iHs"
+        />
+        <ProjectCard 
+          image={placeholder}
+          title="typos"
+          quickdesc="placeholder"
+          codelink="https://github.com/DrewDalmedo/typos"
+          demolink="https://typos-drewdalmedo.web.app"
         />
       </Row>
     </div>
